a23a72e4b8a1ccff65dec73a64cf226dc4c4396f
Howard Mayorga <Howard@howards-mac.lan>
2021-01-12 15:40:10 -0500

    made list for csv file and calculated number of months reported

--- PyBank/main.py
+++ PyBank/main.py
@@ -11,13 +11,21 @@ with open(csvpath) as csvfile:
 
     # CSV reader specifies delimiter and variable that holds contents
     csvreader = csv.reader(csvfile, delimiter=',')
-
+    
     print(csvreader)
+    
 
     # Read the header row first (skip this step if there is now header)
     csv_header = next(csvreader)
-    print(f"CSV Header: {csv_header}")
+    print(csv_header)
+    
 
-    # Read each row of data after the header
+    # # Read each row of data after the header
     # for row in csvreader:
     #     print(row)
+    #     print(len(csvreader))
+   
+    # calculate total number of months in data
+    Total_months = len(list(csvreader))
+    # print(f"Total months = {Total_months}")
+    print(Total_months)
