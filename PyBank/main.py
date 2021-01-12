9b52188ae3c94a7407a9523602d0a6a1dc688046
Howard Mayorga <Howard@howards-mac.lan>
2021-01-12 18:16:53 -0500

    figured out how to get avg change by appending each chnge to new list

--- PyBank/main.py
+++ PyBank/main.py
@@ -24,14 +24,26 @@ with open(csvpath) as csvfile:
     profit_loss = []
     chg_pl = []
 
-    # # Read each row of data after the header
-
+    # # Read each row of data after the header. Create new list for total months and profit/loss
+    
     for row in csvreader:
 
         Total_months.append(row[0])
         profit_loss.append(int(row[1]))    
     
+    # Print out analysis
     print(len(Total_months))
     print(sum(profit_loss))
+    print("Financial Analysis")
+    print("-------------------------------")
+    print("Total Months:", len(Total_months))
+    print("Total: ", sum(profit_loss))
+
+    for i in range(1,len(profit_loss)):
+        chg_pl.append(profit_loss[i] - profit_loss[i-1])
+        avg_chg = sum(chg_pl) / len(chg_pl)
+    print("avg change: ", avg_chg)    
+
+
    
     
