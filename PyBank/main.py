0ac4f37cd6fb518a47a296347ad8afc8eca2713d
Howard Mayorga <Howard@howards-mac.lan>
2021-01-14 16:32:15 -0500

    completed half of assignment (PyBank) and need to start PyPoll

--- PyBank/main.py
+++ PyBank/main.py
@@ -4,9 +4,10 @@ import os
 
 # Module for reading CSV files
 import csv
-
+# Define location of CSV
 csvpath = os.path.join('Resources', 'budget_data.csv')
 
+# Open csv
 with open(csvpath) as csvfile:
 
     # CSV reader specifies delimiter and variable that holds contents
@@ -15,11 +16,11 @@ with open(csvpath) as csvfile:
     print(csvreader)
     
 
-    # Read the header row first (skip this step if there is now header)
+    # Read the header row first (skip this step if there is no header)
     csv_header = next(csvreader)
-    # print(csv_header)
+    print(csv_header)
     
-    # calculate total number of months in data
+    # define variables to hold new lists for solution
     Total_months = []
     profit_loss = []
     chg_pl = []
@@ -32,25 +33,43 @@ with open(csvpath) as csvfile:
         profit_loss.append(int(row[1]))    
     
     # Print out analysis
-    print(len(Total_months))
-    print(sum(profit_loss))
+
     print("Financial Analysis")
     print("-------------------------------")
     print("Total Months:", len(Total_months))
-    print("Total: ", sum(profit_loss))
+    print("Total: ", "$ ", sum(profit_loss))
 
     # loop through the profit/loss list and subtract the next cell from the current cell
     for i in range(1,len(profit_loss)):
         chg_pl.append(profit_loss[i] - profit_loss[i-1])
         avg_chg = sum(chg_pl) / len(chg_pl)
-        
-
-    print("avg change: ", avg_chg)    
     
-    print("Greatest Increase in profits: ",max(profit_loss))
-    print("Greatest Decrease in profits: ",min(profit_loss))
+    # set date variable for greatest increase/decrease in profit/loss    
+    max_date = (Total_months[profit_loss.index(max(profit_loss))])
+    min_date = (Total_months[profit_loss.index(min(profit_loss))])
+    
+    print("avg change: ", round(avg_chg,2))    
+    
+    print("Greatest Increase in profits: ", max_date, "$ ",max(profit_loss))
+    print("Greatest Decrease in profits: ", min_date, "$ ",min(profit_loss))
 
-    print(Total_months[profit_loss.index(max(profit_loss))])
+import sys
+# Save reference to original standard output
+original_stdout = sys.stdout 
+
+with open('Analysis/Analysis.txt', 'w') as f:
+    # change std output to file w
+    sys.stdout = f
+    print("Financial Analysis")
+    print("-------------------------------")
+    print("Total Months:", len(Total_months))
+    print("Total: ", "$ ", sum(profit_loss))
+    print("avg change: ", round(avg_chg,2))    
+    print("Greatest Increase in profits: ", max_date, "$ ",max(profit_loss))
+    print("Greatest Decrease in profits: ", min_date, "$ ",min(profit_loss))
+    sys.stdout = original_stdout
+    
+    
 
 
 
