602dff8649e47b01afd6154f35fcf05d2698ffcb
Howard Mayorga <Howard@howards-mac.lan>
2021-01-15 23:19:49 -0500

    read csv and grab index of candidates

--- PyPoll/testpoll.py
+++ PyPoll/testpoll.py
@@ -5,7 +5,7 @@ import os
 # Module for reading CSV files
 import csv
 # Define location of CSV
-csvpath = os.path.join('Resources', 'election_data.csv')
+csvpath = os.path.join('Resources', 'test.csv')
 
 # Open csv
 with open(csvpath) as csvfile:
@@ -13,26 +13,18 @@ with open(csvpath) as csvfile:
     # CSV reader specifies delimiter and variable that holds contents
     csvreader = csv.reader(csvfile, delimiter=',')
     
-    print(csvreader)
-    
-
     # Read the header row first (skip this step if there is no header)
     csv_header = next(csvreader)
     print(csv_header)
     
-    # define variables to hold new lists for solution
-    vote_count = []
-    candidates = []
+    voter_list = list(csvreader)
+# print(len(voter_list))
+    
+def Extract(voter_list):
+    return [item[2] for item in voter_list]
 
-    # Read each row of data after the header  
-    for row in csvreader:
-         if row[0] == 'Voter ID':
-             pass
-         else:
-             if row[2] not in candidates:
-                 candidates.append(row[2])
-             else:
-                 pass
-
-
-    print(candidates)
+print(Extract(voter_list))    
+    # Read the header row first (skip this step if there is no header)
+    # csv_header = next(csvreader)
+    # print(csv_header)
+    
