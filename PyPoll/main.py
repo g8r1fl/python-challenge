9489ecbd5687e2c6c86035dd98eef5ccd392c109
Howard Mayorga <Howard@howards-mac.lan>
2021-01-16 19:50:41 -0500

    still trying to get the count of votes.

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -7,26 +7,74 @@ import csv
 # Define location of CSV
 csvpath = os.path.join('Resources', 'election_data.csv')
 
+# create blank dict
 # Open csv
 with open(csvpath) as csvfile:
 
     # CSV reader specifies delimiter and variable that holds contents
     csvreader = csv.reader(csvfile, delimiter=',')
-    
     print(csvreader)
     
-
     # Read the header row first (skip this step if there is no header)
     csv_header = next(csvreader)
     print(csv_header)
     
-    voter_list = list(csvreader)
 
-def Extract(voter_list):
-    return [item[2] for item in voter_list]
+    # for loop that reads each row, checkin candidate row[indx]. if key exist increment value, 
+    # if not create key and give a value
+    candidates = []
+    dict ={}
+    keys_list = list(dict)
+    # key = keys_list[]
+    counter = 0
+    # Read each row of data after the header  
+    for row in csvreader:
+         if row[0] == 'Voter ID':
+             pass
+         else:
+             if row[2] not in candidates:
+                 candidates.append(row[2])
+                 dict[row[2]] = ""
+             else:
+                 counter = counter + 1
+                 pass
 
-candidate_list = Extract(voter_list)       
-print(len(candidate_list))
+    Khan_votes = dict.get(list(dict)[0])             
+    print("Election Results")
+    print("------------------------")
+    print("Total Votes: " , counter)
+    print("------------------------")
+    print(list(dict)[0])
+    print(list(dict)[1])
+    print(counter)
+    print(dict)
+    print(Khan_votes)
+    print(counter)
+    # print csvreader to list of lists
+    # voter_list = list(csvreader)
+
+# Extract just the candidates from list
+# def Extract(voter_list):
+#     return [item[2] for item in voter_list]
+# # assign new list to candidates
+# candidates = (Extract(voter_list))       
+
+# # create empty dictionary
+# candidates_dict = {}
+
+# # update 'candidates_dict' with keys for each element (i) in 'candidates'
+# for i in range(len(candidates)):
+#     candidates_dict[candidates[i]] = candidates.count(candidates[i])
+
+# print(candidates_dict) 
+
+
+
+
+
+
+
+
     # define variables to hold new lists for solution
     # vote_count = []
     # candidates = []
--- PyPoll/main_new.py
+++ PyPoll/main_new.py
@@ -7,30 +7,42 @@ import csv
 # Define location of CSV
 csvpath = os.path.join('Resources', 'test.csv')
 
-dict = {}
+# create blank dict
+# election_dict = {}
+
 # Open csv
 with open(csvpath) as csvfile:
 
     # CSV reader specifies delimiter and variable that holds contents
     csvreader = csv.reader(csvfile, delimiter=',')
+    print(csvreader)
     
     # Read the header row first (skip this step if there is no header)
     csv_header = next(csvreader)
     print(csv_header)
+
+    candidates = []
+    dict ={}
+    counter = 0
+# Read each row of data after the header  
+    for row in csvreader:
+         if row[0] == 'Voter ID':
+             pass
+         else:
+             if row[2] not in candidates:
+                 candidates.append(row[2])
+                 dict[row[2]] = ""
+             else:
+                 counter = counter + 1
+                 pass
+
+
+    print(candidates)
+    print(dict)
+    print(counter)
+   
     
-    count = 0
 # for loop that reads each row, checkin candidate row[indx]. if key exist increment value, 
 # if not create key and give a value
-
-    for row in csvreader:
-        if (row[2]) not in dict:
-            dict[row[2]] = 1
-            
-        else:
-            dict[row[2]] = count + 1
-            # count = 0
-
-print(dict)
-print(count)
 
 
